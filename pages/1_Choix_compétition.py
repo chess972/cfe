205851428d8cc53de0e61999594a4c9f5d91cea8
chess972 <echecs972@gmail.com>
2026-01-01 17:44:49 -0400

    Update 1_Choix_compétition.py : added streamlit stub

--- pages/1_Choix_compétition.py
+++ pages/1_Choix_compétition.py
@@ -4,3 +4,8 @@
 
 Ici, on peut choisir la compétition (c-à-d. CFE 2025 ou LFR 2026, etc), pour certains cas.
 """
+import streamlit as st
+from data_engine import get_settings  # Import from your engine, not other pages
+
+st.title("Choix de la Compétition")
+# ... rest of the page code
