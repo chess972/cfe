d3e13d2f356ac5f72fda5ee3283655bc4e8934fa
chess972 <echecs972@gmail.com>
2026-01-01 17:24:08 -0400

    Create Accueil.py

---
+++ Accueil.py
@@ -0,0 +1,12 @@
+""" Accueil.py - main file for streamlit app "CFE/CFT/LFR management
+(c) 2025-2026 by M. F. Hasler
+
+import streamlit as st
+
+# This sets the title in the browser tab
+st.set_page_config(page_title="Gestion CFE/CFT/LFR", layout="wide")
+
+st.title("♟️ Bienvenue dans le script de gestion CFE/CFT/LFR")
+st.write("(c) Oct.2025 - Jan.2026 by MFH")
+
+st.info("Sélectionnez une option dans le menu à gauche pour commencer.")
